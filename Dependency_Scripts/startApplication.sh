6f7df63fdadbc447d25d01d1dd1a7b5ea83d102b
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-11 15:17:09 +0530

    nodejs updated

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/codepipeline/node
 #nohup node index.js > /dev/null 2>&1 &
-pm2 serve build 8080 --spa
+nohup npm start > /dev/null 2>&1 &
+#pm2 serve build 8080 --spa
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('......Hello World......\n');
+}).listen(3000, '0.0.0.0');
+console.log('Server running at http://0.0.0.0:3000/');
